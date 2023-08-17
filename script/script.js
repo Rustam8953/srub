ece6524191b45c867caa44b1b3463a9dfd9a7071
Rustam <78682874+Rustam8953@users.noreply.github.com>
2023-08-17 21:23:30 +0700

    srub

---
+++ script/script.js
@@ -0,0 +1,30 @@
+const headerInfo = document.querySelector('.header-info')
+const headerLine = document.querySelector('.header-line');
+
+window.addEventListener('click', (e) => {
+
+    if(e.target.closest('.header-burger')) {
+        headerInfo.classList.toggle('active')
+        headerLine.classList.toggle('active')
+        document.body.classList.toggle('stuck')
+    }
+
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    mobileBanner();
+})
+
+window.addEventListener('resize', () => {
+    mobileBanner()
+})
+
+
+function mobileBanner() {
+    const banner = document.querySelector('.banner-img img')
+    if (screen.width < 800) {
+        banner.setAttribute('src', './img/banner/mobile.png')
+    } else {
+        banner.setAttribute('src', './img/banner/9 каркас_1 1.png')
+    }
+}
